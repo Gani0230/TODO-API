2fc036a0ed1e8c75eba25a37fb3cb811ed207660
Gani0230 <ganeshjadhav7478@gmail.com>
2025-04-27 19:10:32 +0530

    add cors() to allow data fetching

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { Todo } from './db.js';
-
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cors())
 
 app.post("/",async (req,res)=>{
     const id = req.body.id;
